08cfc1fce17eb816ec29865572a19aed79d9fdd2
ylehilds <lehi_alcantara@byu.edu>
2023-05-08 22:16:38 -0600

    attempt to fix acl issues part 2

--- main.tf
+++ main.tf
@@ -54,7 +54,8 @@ resource "aws_s3_bucket_policy" "policy" {
             "Effect": "Allow",
             "Principal": "*",
             "Action": [
-                "s3:GetObject"
+                "s3:GetObject",
+                "s3:PutObject"
             ],
             "Resource": [
                 "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
