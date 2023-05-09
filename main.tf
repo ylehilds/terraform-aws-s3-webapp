b66f0f4794faef9e754fa0b58e20e9700b7fc1c2
ylehilds <lehi_alcantara@byu.edu>
2023-05-08 22:06:45 -0600

    attempt to fix acl issues

--- main.tf
+++ main.tf
@@ -16,7 +16,28 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket,
+    aws_s3_bucket_public_access_block.bucket,
+  ]
+
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
